b48036e23a210f280c54ab4fde2d1989785f6eb7
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2023-11-15 10:19:08 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
     let a = 0
     const ab = document.getElementById("button")
-    alert("Hello!")
     function Lol(){
       a++;
        localStorage.setItem(a,a)
-      if (a = 5) {Alert("Hello!")} 
+      if (a = 5) {
+          alert("Hello!")
+      } 
     }
