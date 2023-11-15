844704a9513d131a9a1bdb9ab714ffbdce06392b
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2023-11-15 10:16:09 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,8 @@
-function Lol(){
-localStorage.setItem("myCat", "a");
-}
+    let a = 0
+    const ab = document.getElementById("button")
+    Alert("Hello!")
+    function Lol(){
+      a++;
+       localStorage.setItem(a,a)
+      if (a = 5) {Alert("Hello!")} 
+    }
