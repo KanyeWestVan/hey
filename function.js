e3682bfdacb49b51db02c36b5c28ffd35a117878
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2023-11-16 09:11:35 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
     let a = 0
     const ab = document.getElementById("button")
-    ab.style.scale = "5"
+    ab.style.scale = "2 0.7"
 function Lol(){
       a++;
        localStorage.setItem(a,a)
