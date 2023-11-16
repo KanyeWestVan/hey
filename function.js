083ef7fd27e8b857d11587f10646bf834189b640
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2023-11-16 10:42:12 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,18 @@
     let a = 0
     const ab = document.getElementById("B45")
 //    ab.style.scale = "2 0.7"
+    
+
+    
 function Lol(){
       a++;
        localStorage.setItem(a,a)
-    if (a < 5) {
+    if (a > 5) {
           alert("Hello!")
-      } 
+                } 
+            }       
+
+    function Looptest(){
+
+
     }
-function Looptest(){
-
-
-}
