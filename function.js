78afb81c173c634e2c0011b3d5a89f8ef343b941
KanyeWestVan <125940103+KanyeWestVan@users.noreply.github.com>
2023-11-16 10:36:59 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
     let a = 0
     const ab = document.getElementById("B45")
-    ab.style.scale = "2 0.7"
+//    ab.style.scale = "2 0.7"
 function Lol(){
       a++;
        localStorage.setItem(a,a)
